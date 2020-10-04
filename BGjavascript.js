12abab50b97252211699dfb3577c9cdc1a6ec2cc
Chiemeka Adingwupu <pauladingwupu@gmail.com>
2020-10-04 11:30:27 +0100

    adding new files

---
+++ BGjavascript.js
@@ -0,0 +1,29 @@
+
+var h3 = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 =  document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+
+
+
+
+
+function setGradientColors(){
+    
+    body.style.background = "linear-gradient(to right," + color1.value +","+color2.value+")";
+
+    CSS.textContent = body.style.background + ":";
+
+  
+}
+
+color1.addEventListener('input', setGradientColors);
+
+color2.addEventListener('input', setGradientColors);
+
+
+
+   
+
+
